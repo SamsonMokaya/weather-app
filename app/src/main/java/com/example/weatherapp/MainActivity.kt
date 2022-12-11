8dbb9c5344e041f9d5001750db71d40a7b5a7f16
SamsonMokaya <mokayasamson950@gmail.com>
2022-12-11 13:14:26 -0800

    With errors

--- app/src/main/java/com/example/weatherapp/MainActivity.kt
+++ app/src/main/java/com/example/weatherapp/MainActivity.kt
@@ -2,10 +2,30 @@ package com.example.weatherapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.ProgressBar
+import android.widget.RelativeLayout
+import android.widget.TextView
+import com.google.android.material.textfield.TextInputEditText
+import org.w3c.dom.Text
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+         val homeRL = findViewById<RelativeLayout>(R.id.idRLHome);
+         val loadingPB = findViewById<ProgressBar>(R.id.idPBLoading);
+         val cityNameTV = findViewById<TextView>(R.id.idTVCityName);
+         val temperatureTV = findViewById<TextView>(R.id.idTVTemperature);
+         val conditionTV = findViewById<TextView>(R.id.idTVCondition);
+         val weatherTV = findViewById<TextView>(R.id.idRVWeather);
+         val cityEdt = findViewById<TextInputEditText>(R.id.idEdtCity);
+         val backIV = findViewById<ImageView>(R.id.idIVBlack);
+         val iconIV = findViewById<ImageView>(R.id.idIVIcon);
+         val searchIV = findViewById<ImageView>(R.id.idIVSearch);
+
+
     }
 }
---
+++ app/src/main/java/com/example/weatherapp/WeatherRVAdapter.java
@@ -0,0 +1,60 @@
+package com.example.weatherapp;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+
+public class WeatherRVAdapter extends RecyclerView.Adapter<WeatherRVAdapter.ViewHolder> {
+
+    private Context context;
+    private ArrayList<WeatherRVModel> weatherRVModelArrayList;
+
+    public WeatherRVAdapter(Context context, ArrayList<WeatherRVModel> weatherRVModelArrayList) {
+        this.context = context;
+        this.weatherRVModelArrayList = weatherRVModelArrayList;
+    }
+
+    @NonNull
+    @Override
+    public WeatherRVAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.weather_rv_item,parent,false);
+
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+
+        Picasso.get().load();
+    }
+
+    @Override
+    public int getItemCount() {
+        return weatherRVModelArrayList.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+
+        private TextView windTV, temperatureTV,timeTV;
+        private ImageView conditionIV;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            windTV = itemView.findViewById(R.id.idTVWindSpeed);
+            temperatureTV= itemView.findViewById(R.id.idTVTemparture);
+            timeTV = itemView.findViewById(R.id.idTVTime);
+            conditionIV = itemView.findViewById(R.id.idIVCondition);
+        }
+    }
+}
